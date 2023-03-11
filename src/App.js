9a4c0b39df92417b2094613ad3f828bf4aa9061e
Waquar Mahboob <waquarmahboob4@gmail.com>
2023-03-11 14:04:02 +0530

    Addityional Offer Task done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import AdditionalOffers from './Component/Additional Offers/AdditionalOffers';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <AdditionalOffers data={[{name:'Axis Bank',image:'https://www.axisbank.com/images/default-source/gallery/gallery_img1.jpg',content:' Flat 25% off* at INOX on Movie tickets and Food & Beverages Master Card of Axis Bank'},{name:'Eazy Diner',image:'https://d4t7t8y8xqo0t.cloudfront.net/resized/120X120/author%2Feditorial-team-20170314105334.jpg',content:' Flat 25% off* at INOX on Movie tickets and Food & Beverages Master Card of Axis Bank'},{name:'Axis Bank',image:'https://www.axisbank.com/images/default-source/gallery/gallery_img1.jpg',content:' Flat 25% off* at INOX on Movie tickets and Food & Beverages Master Card of Axis Bank'},{name:'Eazy Diner',image:'https://d4t7t8y8xqo0t.cloudfront.net/resized/120X120/author%2Feditorial-team-20170314105334.jpg',content:' Flat 25% off* at INOX on Movie tickets and Food & Beverages Master Card of Axis Bank'}]}/>
     </div>
   );
 }
--- src/Component/Additional Offers/AdditionalOffers.js
+++ src/Component/Additional Offers/AdditionalOffers.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import "./index.css";
+import axisImg from "../assets/gallery_img1.jpg";
 
-export default function AdditionalOffers() {
+export default function AdditionalOffers({data}) {
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="row">
+      <h1 style={{ flex: "100%" }}>Additional Offers</h1>
+      {
+        data.map((element,index)=>(
+          <div key={index} className="card left">
+            <div className="img">
+              <img src={element.image} alt="axis" />
+            </div>
+            <div className="card-container">
+              <h4>
+                <b>{element.name}</b>
+              </h4>
+              <p className="word">
+                {element.content}
+              </p>
+            </div>
+          </div>
+
+
+        ))
+      }
+
+      {Array(10)
+        .fill()
+        .map((element, i) => (
+          <div key={i} className="card left">
+            <div className="img">
+              <img src={axisImg} alt="axis" />
+            </div>
+            <div className="card-container">
+              <h4>
+                <b>Axis Bank</b>
+              </h4>
+              <p>
+                Flat 25% off* at INOX on Movie tickets and 
+                Food & Beverages
+                Master Card of Axis Bank
+              </p>
+            </div>
+          </div>
+        ))}
     </div>
-  )
+  );
 }
